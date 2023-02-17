da4b7142d1cf68812da9a50e1ed3fb3613d28cbb
billbo24 <wfloyd1231@gmail.com>
2023-02-16 22:05:15 -0500

    Initial Commit

---
+++ JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by William Floyd on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
